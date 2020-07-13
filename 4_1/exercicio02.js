d94b404561806f0365bff20b9639ff81e83d61dd
Lais Gurgel <laisgurgelf@gmail.com>
2020-07-13 19:36:21 -0300

    Alteraçao de console.log com retorno dos dois numeros e comparaçao

---
+++ 4_1/exercicio02.js
@@ -0,0 +1,10 @@
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+let a = 9;
+let b = 8;
+
+if (a>b) {
+    console.log((a) + " é maior do que: "+ (b));
+} else {
+    console.log((b) + " é maior do que: "+ (a));
+}
