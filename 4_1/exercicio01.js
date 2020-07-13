28195d50a7ef83fd3a295044692202dfcd1a02c6
Lais Gurgel <laisgurgelf@gmail.com>
2020-07-13 19:45:01 -0300

    Criaçao exercicio 01 - Programa operaçoes aritmeticas basicas

---
+++ 4_1/exercicio01.js
@@ -0,0 +1,10 @@
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Faça programas para: Adição, Subtracão, Multiplicação, Divisão e Módulo
+
+let a = 3;
+let b = 6;
+
+console.log('Adição: ' + (a+b));
+console.log('Subtração: ' + (a-b));
+console.log('Multiplicação: ' + (a*b));
+console.log('Divisão: ' + (a/b));
+console.log('Módulo: ' + (a%b));
