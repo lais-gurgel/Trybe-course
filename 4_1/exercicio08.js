0c3a72193e1c5c5c14b10b225a566b585e599c67
Lais Gurgel <laisgurgelf@gmail.com>
2020-07-13 22:05:45 -0300

    Criaçao exercicio 08 - Programa compara tres numeros e retorna se existem numeros pares ou nao

---
+++ 4_1/exercicio08.js
@@ -0,0 +1,21 @@
+// Escreva um programa que defina três números em variáveis no seu começo e retorne true se uma das três for par. Caso contrário, ele retorna false.
+// Bonus: use somente um if.
+
+let a = 1;
+let b = 2;
+let c = 3;
+
+if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0 ) {
+    console.log('true')
+} else {
+    console.log('false')
+}
+
+//ou 
+
+let result = false;
+
+if ((a % 2 === 0 || b % 2 === 0 || c % 2 === 0)) {
+    result = true;
+  };
+  console.log(result);
