2c2c629d32eb23a8d7c877ce805c93863ad6a92d
Lais Gurgel <laisgurgelf@gmail.com>
2020-07-14 15:21:27 -0300

    Ediçao de anotaçoes

--- 4_2/exercicio01.js
+++ 4_2/exercicio01.js
@@ -1,10 +1,4 @@
 // Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
-// Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
-// Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
-// Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
-// Utilizando for, descubra qual o maior valor contido no array e imprima-o;
-// Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
-// Utilizando for, descubra qual o menor valor contido no array e imprima-o;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
