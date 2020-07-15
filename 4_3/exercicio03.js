40d2a503fc23c92d00df2c0b1fce42df0e7b8651
Lais Gurgel <laisgurgelf@gmail.com>
2020-07-15 18:35:38 -0300

    Adicao de exercicio03.js com enunciado e descricao de BABY STEPS'

---
+++ 4_3/exercicio03.js
@@ -0,0 +1,5 @@
+// Faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um triangulo feito de asteriscos de base e lateral DIREITA de tamanho n.
+// BABY STEPS
+// Adição de variaveis n, de espaço e asterisco
+// Adição de loop com impressão de linha com espaço e asteriscos
+// Adição de loop com repetição de impressao de linha
