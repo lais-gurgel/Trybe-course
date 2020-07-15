1a673a460c1d6d1cba4a85567abc23be1e841c08
Lais Gurgel <laisgurgelf@gmail.com>
2020-07-15 16:08:37 -0300

    Adiçao de enunciado

---
+++ 4_3/exercicio00.js
@@ -0,0 +1,3 @@
+// Utilizando o array abaixo, percorra-o somando todos os valores. Caso o valor final seja maior que 15, imprima-o. Caso seja igual ou menor que 15, imprima a mensagem: "valor menor que 16":
+// Utilize o metodo BABY STEPS fazendo commits a cada passo executado.
+
