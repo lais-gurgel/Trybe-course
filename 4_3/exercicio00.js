17b86c1a9d6ac7f8056901fd22b009bba4794692
Lais Gurgel <laisgurgelf@gmail.com>
2020-07-15 17:37:17 -0300

    Adiçao de array com valores

--- 4_3/exercicio00.js
+++ 4_3/exercicio00.js
@@ -1,3 +1,4 @@
 // Utilizando o array abaixo, percorra-o somando todos os valores. Caso o valor final seja maior que 15, imprima-o. Caso seja igual ou menor que 15, imprima a mensagem: "valor menor que 16":
 // Utilize o metodo BABY STEPS fazendo commits a cada passo executado.
 
+let fruits = [3, 4, 10, 1, 12];
