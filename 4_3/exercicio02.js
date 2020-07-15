af3ef4bf63fa861331c8ce748b995b9a59e8c7c9
Lais Gurgel <laisgurgelf@gmail.com>
2020-07-15 18:24:55 -0300

    Adicao de metodologia BABY STEPS e criacao de variaveis

--- 4_3/exercicio02.js
+++ 4_3/exercicio02.js
@@ -1,1 +1,10 @@
 // Faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um triangulo feito de asteriscos de base e lateral esquerda de tamanho n.
+//BABY STEPS
+// Adicao de variavel n, variavel asterisco
+// Criacao de loop para adicionar "*" n vezes
+// Criacao de loop para imprimir "*" a cada simbolo adicionado
+
+let n = 5;
+let linhaAsteriscos = "";
+let asterisco = "*";
+
