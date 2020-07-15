a19b46dddcd60c0d3f296f069368111e241ea482
Lais Gurgel <laisgurgelf@gmail.com>
2020-07-15 15:53:33 -0300

    Criaçao de exercicio00.js - estudo de BABY STEPS em logica de programacao

---
+++ 4_3/exercicio00.js
@@ -0,0 +1,3 @@
+// Utilizando o array abaixo, percorra-o somando todos os valores. Caso o valor final seja maior que 15, imprima-o. Caso seja igual ou menor que 15, imprima a mensagem: "valor menor que 16": 
+// let fruits = [3, 4, 10, 1, 12];
+// Utilizando o método 'baby steps', faça commit a cada tarefa executada
