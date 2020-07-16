18d491a3d0c0524905c588157202512008f003da
Lais Gurgel <laisgurgelf@gmail.com>
2020-07-16 19:33:17 -0300

    Adicao de chave e valor

--- 4_4/exercicio03.js
+++ 4_4/exercicio03.js
@@ -4,6 +4,7 @@ let info = {
     personagem: "Margarida",
     origem: "Pato Donald",
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim",
   };
 
 for (let key in info) {
