f58f4fc29513a31ff7987801d02bb10718a03b9a
Lais Gurgel <laisgurgelf@gmail.com>
2020-07-16 13:50:06 -0300

    Adiçao de comentarios BABY STEPS e criacao de variaveis iniciais

--- 4_3/exercicio04.js
+++ 4_3/exercicio04.js
@@ -1,1 +1,6 @@
 // Faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela uma pirâmide com n asteriscos de base.
+//BABY STEPS
+// Criação de variavel N, e simbolo a ser impresso
+
+let base = 5;
+let symbol = "*";
