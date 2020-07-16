c6376e9cb51b76db963f96aa71a33ffeb8ca229d
Lais Gurgel <laisgurgelf@gmail.com>
2020-07-16 13:51:41 -0300

    Criacao de FOR para percorrer a matriz e variaveis correspondentes

--- 4_3/exercicio04.js
+++ 4_3/exercicio04.js
@@ -1,6 +1,15 @@
 // Faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela uma pirâmide com n asteriscos de base.
 //BABY STEPS
 // Criação de variavel N, e simbolo a ser impresso
+// Criacao de for para percorrer a matriz e variaveis correspondentes
 
 let base = 5;
 let symbol = "*";
+let lineIndex;
+let columnIndex;
+
+for (lineIndex = 0; ; lineIndex += 1) {
+    for (columnIndex = X; regra; columnIndex += 1) {
+
+    }
+}
