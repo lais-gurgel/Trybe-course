96e5f68fafdcf0ef9310dada4cf7b9b172ed49f1
Lais Gurgel <laisgurgelf@gmail.com>
2020-07-16 19:56:22 -0300

    Adiçao de pasta

---
+++ 4_3/rascunho04.js
@@ -0,0 +1,15 @@
+// Faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela uma pirâmide com n asteriscos de base.
+//BABY STEPS
+// Criação de variavel N, e simbolo a ser impresso
+// Criação de FOR para percorrer a matriz da linha e da coluna, e variaveis para linha e coluna
+
+let n = 5;
+let symbol = "*"
+let lineIndex;
+let columnIndex;
+let lineInput;
+
+let midOfMatrix = 
+
+
+
