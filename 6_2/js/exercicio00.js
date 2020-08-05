dce9b219975a8ffaafdc69b9783651d94b0573a6
Lais Gurgel <laisgurgelf@gmail.com>
2020-08-05 15:10:29 -0300

    Adicionando pikaday e formatando retorno da data

--- 6_2/js/exercicio00.js
+++ 6_2/js/exercicio00.js
@@ -1,3 +1,26 @@
+window.onload = function () {
+  var picker = new Pikaday({
+    field: document.getElementById('datepicker'),
+    format: 'DD/MM/YYYY',
+    toString(date, format = 'DD/MM/YYYY') {
+        // you should do formatting based on the passed format,
+        // but we will just return 'D/M/YYYY' for simplicity
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    },
+    parse(dateString, format = 'DD/MM/YYYY') {
+        // dateString is the result of `toString` method
+        const parts = dateString.split('/');
+        const day = parseInt(parts[0], 10);
+        const month = parseInt(parts[1], 10) - 1;
+        const year = parseInt(parts[2], 10);
+        return new Date(day, month, year);
+    }
+  });
+}
+
 // Utilize estruturas de repetição via JavaScript para gerar os <option>
 const state = document.querySelector('#estado');
 const states = ['Selecione seu estado', 'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
