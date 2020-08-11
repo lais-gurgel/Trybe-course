b84075b5ba3c639b327c11ac42c841eef467500c
Lais Gurgel <laisgurgelf@gmail.com>
2020-08-11 15:43:52 -0300

    exercicio00.js funçao para adicionar novo objeto com chave e valor

---
+++ 7_2/exercicio00.js
@@ -0,0 +1,5 @@
+// crie uma função que receba três parâmetros, sendo eles: um objeto, o nome de uma chave e o seu valor. O retorno dessa função deve ser o objeto já com a nova chave adicionada nele.
+
+const newObjectWithKey = (nameObject, nameKey, valueKey) => {
+  nameObject[nameKey] = valueKey;
+}
