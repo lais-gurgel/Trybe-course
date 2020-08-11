fb57a83f4645e825e45057ccd8c848f6cf065108
Lais Gurgel <laisgurgelf@gmail.com>
2020-08-11 19:37:02 -0300

    Resoluçao

--- 7_2/exercicio00.js
+++ 7_2/exercicio00.js
@@ -1,6 +1,9 @@
 // crie uma função que receba três parâmetros, sendo eles: um objeto, o nome de uma chave e o seu valor. O retorno dessa função deve ser o objeto já com a nova chave adicionada nele.
-let newObject = {};
+const newObject = {};
 
 const addKey = (nameObject, nameKey, valueKey) => {
   nameObject[nameKey] = valueKey;
+  console.log(nameObject)
 }
+
+addKey(newObject, 'Key', 'Value of key');
