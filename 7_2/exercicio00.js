58d56ace6a92b988af2df9fa4241cd7c8c1407f3
Lais Gurgel <laisgurgelf@gmail.com>
2020-08-11 15:57:17 -0300

    Reformulando funçao

--- 7_2/exercicio00.js
+++ 7_2/exercicio00.js
@@ -1,5 +1,6 @@
 // crie uma função que receba três parâmetros, sendo eles: um objeto, o nome de uma chave e o seu valor. O retorno dessa função deve ser o objeto já com a nova chave adicionada nele.
+let newObject = {};
 
-const newObjectWithKey = (nameObject, nameKey, valueKey) => {
+const addKey = (nameObject, nameKey, valueKey) => {
   nameObject[nameKey] = valueKey;
 }
