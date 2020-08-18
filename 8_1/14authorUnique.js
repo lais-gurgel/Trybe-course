8328e4b519099e04458db281b5defd212bb23cb3
Lais Gurgel <laisgurgelf@gmail.com>
2020-08-17 22:10:35 -0300

    Exercicios

--- 8_1/14authorUnique.js
+++ 8_1/14authorUnique.js
@@ -63,15 +63,12 @@ const books = [
   },
 ];
 
-const expected_result = true
-
-// Faça uma função que retorne true, se algum livro foi lançado na década de 80, e false, caso contrário.
-
-function someBookWasReleaseOnThe80s() {
+const expected_result = false;
+// Faça uma função que retorne true, caso nenhum author tenha nascido no mesmo ano, e false, caso contrário.
+function authorUnique() {
   // escreva seu código aqui
-  return books.some(book => (
-    book.releaseYear >= 1980 && book.releaseYear <= 1989
-  ))
+  
+
 }
 
-assert.equal(someBookWasReleaseOnThe80s(), expected_result);
+assert.equal(authorUnique(), expected_result);
