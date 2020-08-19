edad814b712f88308fa475cdc33a9a90ace41c7b
Lais Gurgel <laisgurgelf@gmail.com>
2020-08-18 21:48:38 -0300

    Exercicios

--- 8_2/09nameAndAgeMapSort.js
+++ 8_2/09nameAndAgeMapSort.js
@@ -95,6 +95,8 @@ const expected_result = [
 
 function nameAndAge() {
   // escreva seu código aqui
+  return books.map(book => ({ age: (book.releaseYear - book.author.birthYear), author: book.author.name}))
+  .sort((firstAuthor, secondAuthor) => firstAuthor.age - secondAuthor.age);
 }
 
 assert.deepEqual(nameAndAge(), expected_result);
