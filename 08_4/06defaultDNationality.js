cc78ded3e79bfc218a5563489a682d2f25a02346
Lais Gurgel <laisgurgelf@gmail.com>
2020-08-21 16:45:16 -0300

    exercicios

--- 08_4/06defaultDNationality.js
+++ 08_4/06defaultDNationality.js
@@ -1,5 +1,5 @@
-
-const nationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`
+// Ajuste a arrow function nationality para que a chamada de nationality(person) retorne João is Brazilian.
+const nationality = ({ firstName, nationality }) => (nationality === undefined ? `${firstName} is Brazilian` : `${firstName} is ${nationality}`)
 
 const person = {
     firstName: "João",
@@ -11,8 +11,6 @@ const otherPerson = {
     lastName: "Ivanovich",
     nationality: "Russian"
 }
-
-const { nationality = "Brazilian" } = person;
 
 console.log(nationality(otherPerson)) // Ivan is Russian
 console.log(nationality(person))
