7e87ea0537a8464f0e607022bf0b1f448c152207
Lais Gurgel <laisgurgelf@gmail.com>
2020-09-04 15:36:03 -0300

    Exercicios

--- 10_3/00mock.js
+++ 10_3/00mock.js
@@ -1,3 +1,8 @@
-const randomNumber = () => Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+const randomNumber = () => Math.floor(Math.random() * 100);
 
 const isDivisible = (number) => (randomNumber() % number) === 0;
+
+test('isDivisible', () => {
+  service.randomNumber = jest.fn();
+  
+});
