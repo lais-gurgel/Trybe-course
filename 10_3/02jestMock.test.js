59eafe77cf4d19cb1f555f6fa2501a264ab235a4
Lais Gurgel <laisgurgelf@gmail.com>
2020-09-09 13:09:21 -0300

    Exercicios

---
+++ 10_3/02jestMock.test.js
@@ -0,0 +1,17 @@
+// Mockando Modulos
+// Diferente do jest.fn(), que simula apenas uma função por vez, temos o jest.mock(), que tem como principal diferencial mockar todo um pacote de dependências ou módulo de uma vez.
+
+const math = require('./math');
+jest.mock("./math");
+
+test("#somar", () => {
+  // Aqui testamos se função foi chamada, quantas vezes foi chamada, quais parâmetros foram usados e qual seu retorno
+
+  math.somar.mockImplementation((a, b) => a + b);
+  math.somar(1, 2);
+
+  expect(math.somar).toHaveBeenCalled();
+  expect(math.somar).toHaveBeenCalledTimes(1);
+  expect(math.somar).toHaveBeenCalledWith(1, 2);
+  expect(math.somar(1, 2)).toBe(3);
+});
