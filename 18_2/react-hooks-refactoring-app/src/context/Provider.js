73500cedb83f24826ca332ccf665799113f624f8
Lais Gurgel <laisgurgelf@gmail.com>
2021-02-11 11:23:01 -0300

    exercises

---
+++ 18_2/react-hooks-refactoring-app/src/context/Provider.js
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import CarsContext from './CarsContext';
+
+function Provider({ children }) {
+  const [cars, setCars] = useState({
+    red: false,
+    blue: false,
+    yellow: false,
+  });
+
+  const [signal, setSignal] = useState({ color: 'red' });
+
+  const moveCar = (car, side) => {
+    setCars({
+      ...cars,
+      [car]: side,
+    });
+  };
+
+  const changeSignal = (signalColor) => {
+    setSignal({
+      color: signalColor,
+    });
+  };
+
+  const context = {
+    cars,
+    signal,
+    moveCar,
+    changeSignal,
+  };
+
+  return (
+    <CarsContext.Provider value={context}>
+      {children}
+    </CarsContext.Provider>
+  );
+};
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;
